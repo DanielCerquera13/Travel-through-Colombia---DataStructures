2491d9474db5c8a03fdbf799d7d16f0ba45a1732
velez <velez@172.30.195.26>
2018-11-16 14:55:29 -0500

    Class of methods generics

--- DataStructuresProject/src/dataStructures/MethodsGraphs.java
+++ DataStructuresProject/src/dataStructures/MethodsGraphs.java
@@ -1,12 +1,12 @@
 package dataStructures;
 
-public class MethodsGraphs <T extends comparable, E extends comparable>{
+public class MethodsGraphs <T extends Comparable<T>, E extends Comparable<E>>{
 
 	
 	
 	
 	
-public Vertex[] prim(GraphByLists<T,E> graph, int v) {
+public Vertex<T>[] prim(GraphByLists<T,E> graph, int v) {
 	
 	int[] d=new int[graph.] 
 	
