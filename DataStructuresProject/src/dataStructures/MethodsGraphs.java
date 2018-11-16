356e551c9fd333b15fb26940ae08cfe8346b4fca
velez <velez@172.30.195.26>
2018-11-16 15:46:33 -0500

    Some modification to graphBuMatrix (Add edges to each vertex)

--- DataStructuresProject/src/dataStructures/MethodsGraphs.java
+++ DataStructuresProject/src/dataStructures/MethodsGraphs.java
@@ -1,18 +1,44 @@
 package dataStructures;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class MethodsGraphs <T extends Comparable<T>, E extends Comparable<E>>{
 
 	
+
+	public MethodsGraphs(){
+		
+		
+	}
+	
+	
+	public ArrayList<Vertex<T>> DFS(GraphByMatrix<T, E> g, Vertex<T> v){
+		
+	ArrayList<Vertex<T>> vertices = g.getVertices();	
+	boolean[] visited = new boolean[g.getNumVertex()];
+	Stack<Vertex<T>> stack = new Stack<Vertex<T>>();
+	ArrayList<Vertex<T>> dfs = new ArrayList<>();
 	
 	
 	
-public Vertex<T>[] prim(GraphByLists<T,E> graph, int v) {
 	
-	int[] d=new int[graph.] 
 	
-			
+	return dfs;
+		
+	}
 	
-}	
+	
+	
+	
+	
+//public Vertex<T>[] prim(GraphByLists<T,E> graph, int v) {
+//	
+//	int[] d=new int[graph.] 
+//	
+//			
+//	
+//}	
 	
 	
 	
