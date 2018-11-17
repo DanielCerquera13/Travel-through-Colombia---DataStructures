1cbcd75f6dfa992ebcb427446dd82122efa1001a
velez <velez@172.30.195.26>
2018-11-16 22:45:47 -0500

    DFS has been implemented.

--- DataStructuresProject/src/dataStructures/MethodsGraphs.java
+++ DataStructuresProject/src/dataStructures/MethodsGraphs.java
@@ -9,30 +9,30 @@ public class MethodsGraphs<T extends Comparable<T>, E extends Comparable<E>> {
 
 	}
 
+	// DFS by graph by Matrix Aux
 	private ArrayList<Vertex<T>> DFS(GraphByMatrix<T, E> g, Vertex<T> v, boolean[] visited, Stack<Vertex<T>> stack,
 			ArrayList<Vertex<T>> dfs) {
 
-		if (stack.isEmpty()) {
+		if (dfs.size() == g.getVertices().size()) {
 			return dfs;
 		}
 
 		ArrayList<Vertex<T>> vertices = g.getVertices();
 
 		int index = g.getIndexVertex(v.getValue());
+		visited[index] = true;
 
 		for (int i = 0; i < vertices.get(index).getEdges().size(); i++) {
 
 			if (stack.peek() != v) {
 				stack.push(v);
+				dfs.add(v);
 
 			}
 
 			if (visited[g
 					.getIndexVertex((T) vertices.get(index).getEdges().get(i).getDestination().getValue())] == false) {
 
-				visited[index] = true;
-				System.out.println(v);
-				dfs.add(v);
 				return DFS(g, (Vertex<T>) vertices.get(index).getEdges().get(i).getDestination(), visited, stack, dfs);
 
 			}
@@ -45,6 +45,7 @@ public class MethodsGraphs<T extends Comparable<T>, E extends Comparable<E>> {
 
 	}
 
+	// DFS by graph by Matrix 
 	public ArrayList<Vertex<T>> DFS(GraphByMatrix<T, E> g, Vertex<T> v) {
 
 		boolean[] visited = new boolean[g.getVertices().size()];
@@ -55,18 +56,62 @@ public class MethodsGraphs<T extends Comparable<T>, E extends Comparable<E>> {
 		return DFS(g, v, visited, stack, dfs);
 
 	}
+	
+	
+	// DFS by graph Lists Aux
+	private ArrayList<Vertex<T>> DFS(GraphByLists<T, E> g, Vertex<T> v, boolean[] visited, Stack<Vertex<T>> stack,
+			ArrayList<Vertex<T>> dfs) {
 
-//public Vertex<T>[] prim(GraphByLists<T,E> graph, int v) {
-//	
-//	int[] d=new int[graph.] 
-//	
-//			
-//	
-//}	
+		if (dfs.size() == g.getVertices().size()) {
+			return dfs;
+		}
+
+		ArrayList<Vertex<T>> vertices = g.getVertices();
+
+		int index = g.getIndexVertex(v.getValue());
+		visited[index] = true;
+
+		for (int i = 0; i < vertices.get(index).getEdges().size(); i++) {
+
+			if (stack.peek() != v) {
+				stack.push(v);
+				dfs.add(v);
+
+			}
+
+			if (visited[g
+					.getIndexVertex((T) vertices.get(index).getEdges().get(i).getDestination().getValue())] == false) {
+
+				return DFS(g, (Vertex<T>) vertices.get(index).getEdges().get(i).getDestination(), visited, stack, dfs);
+
+			}
+
+		}
+
+		stack.pop();
+
+		return DFS(g, stack.peek(), visited, stack, dfs);
+
+	}
+	
+	// DFS by Graph by lists.
+	public ArrayList<Vertex<T>> DFS(GraphByLists<T, E> g, Vertex<T> v) {
+
+		boolean[] visited = new boolean[g.getVertices().size()];
+		Stack<Vertex<T>> stack = new Stack<Vertex<T>>();
+		ArrayList<Vertex<T>> dfs = new ArrayList<>();
+		stack.push(v);
+		dfs.add(v);
+		return DFS(g, v, visited, stack, dfs);
+
+	}
+	
+
+	
 
 	public static void main(String[] args) {
 
-		GraphByMatrix<Integer, Integer> g = new GraphByMatrix<>(5);
+		GraphByLists<Integer, Integer> g = new GraphByLists<>(5);
 
 		Integer trece = 13;
 		Integer ocho = 8;
@@ -81,17 +126,18 @@ public class MethodsGraphs<T extends Comparable<T>, E extends Comparable<E>> {
 		g.addVertex(cinco);
 
 		g.addEdge(trece, ocho, false, 0, 1);
+
 		g.addEdge(trece, cinco, false, 0, 1);
 		g.addEdge(trece, v1, false, 0, 1);
+
 		g.addEdge(ocho, nueve, false, 0, 1);
-		g.addEdge(nueve, v1, false, 0, 1);
 		g.addEdge(v1, cinco, false, 0, 1);
 		System.out.println(g.getVertices());
 		System.out.println(g.getVertices().get(4).getEdges().size());
 
 		MethodsGraphs<Integer, Integer> m = new MethodsGraphs<>();
 
-//		System.out.println(m.DFS(g, new Vertex<Integer>(trece)));
+		System.out.println(m.DFS(g, new Vertex<Integer>(trece)));
 
 	}
 
